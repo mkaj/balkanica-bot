887ded6c65a00254a0fecdd85478bb0677594b74
nvllvs <nvllvs@users.noreply.github.com>
2016-11-22 18:01:34 +0100

    Fixed pointless code duplication
    * Fixed unnecessary duplication
    
    * Fixed code style

--- balkanica-bot-twitter.py
+++ balkanica-bot-twitter.py
@@ -1,73 +1,22 @@
-import PIL, tweepy, os, time
-from PIL import Image, ImageDraw, ImageFont
-from random import randint
+import tweepy
+from balkanicagenerator import *
 
-newlineCharacters = 25
-phrasesFileName = 'balkanica.txt'
-res = (560,300)
-legoManRes = (165,300)
-statusList = ['kobiety wino taniec spiew', 'czego ty nie chcesz sam', 'bedzie sie dzialo']
-saveLocation = 'post.jpg'
+saveLocation = 'result.jpg'
 
-
-# loads images in directory
-def getAllImagesInDirectory():
-    fileList = []
-    for dirpath, dirnames, filenames in os.walk('.'):
-        for filename in [f for f in filenames \
-                         if f.endswith('.jpg') \
-                         or f.endswith('.png')]:
-            fileList.append(filename)
-    return fileList
-
-# gets contents of file
-def getContentsOfFile(filename):
-    with open(filename) as file:
-        content = file.read().splitlines()
-    return content
-
-# takes random line from phrasesFileName
-def getPhrase():
-    contents = getContentsOfFile(phrasesFileName)
-    return contents[randint(0,len(contents)-1)]
-
-# randomly chooses lego man image
-def getRandomMan(fileList):
-    filename = fileList[randint(0,len(fileList)-1)]
-    result = Image.open(filename)
-    return result
-
-# places lego man on the left side of the blank image
-def pasteMan():
-    blankImage = Image.new('RGB',res,'white')
-    legoMan = getRandomMan(getAllImagesInDirectory())
-    legoMan = legoMan.resize(legoManRes)
-    blankImage.paste(legoMan,(0,0,legoManRes[0],legoManRes[1]))
-    return blankImage
-
-# crappy text wrap function
-# TODO: stop splitting words
-def convertToFit(phrase):
-    for counter, char in enumerate(phrase):
-        if counter % newlineCharacters == 0 and counter != 0:
-            phrase = phrase[:counter] + '\n' + phrase[counter:]
-    return phrase
-
-# draws text on image
-def makeImage():
+def saveImage():
     img = pasteMan()
     phrase = getPhrase()
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("segoesc.ttf", 25)
     draw.text((150,10), convertToFit(phrase), font=font, fill=(0,0,0,0))
+    
     img.save(saveLocation)
 
-# uploads the image to twitter with a random status from statusList
+# uploads the image to twitter with a random status from text file
 # fill up manually CONSUMER_KEY, CONSUMER_SECRET etc.
 def uploadImage():
-    status = statusList[randint(0,len(statusList)-1)]
+    status = getPhrase()
 
-    imageLocation = 'post.jpg'
     CONSUMER_KEY = 'key'
     CONSUMER_SECRET = 'secret'
     ACCESS_KEY = 'token'
@@ -76,9 +25,13 @@ def uploadImage():
     auth.secure = True
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
-    fn = os.path.abspath(imageLocation)
+    fn = os.path.abspath(saveLocation)
     api.update_with_media(fn, status=status)
 
+def deleteImage():
+    os.remove(saveLocation)
+
 if __name__ == '__main__':
-    makeImage()
+    saveImage()
     uploadImage()
+    deleteImage()
---
+++ balkanicagenerator.py
@@ -0,0 +1,62 @@
+import PIL, os
+from PIL import Image, ImageDraw, ImageFont
+from random import randint
+
+newlineCharacters = 25
+phrasesFileName = 'balkanica.txt'
+res = (560,300)
+legoManRes = (165,300)
+
+
+# loads images in directory
+def getAllImagesInDirectory():
+    fileList = []
+    for dirpath, dirnames, filenames in os.walk('.'):
+        for filename in [f for f in filenames \
+                         if f.endswith('.jpg') \
+                         or f.endswith('.png')]:
+            fileList.append(filename)
+    return fileList
+
+# gets contents of file
+def getContentsOfFile(filename):
+    with open(filename) as file:
+        content = file.read().splitlines()
+    return content
+
+# takes random line from phrasesFileName
+def getPhrase():
+    contents = getContentsOfFile(phrasesFileName)
+    return contents[randint(0,len(contents)-1)]
+
+# randomly chooses lego man image
+def getRandomMan(fileList):
+    filename = fileList[randint(0,len(fileList)-1)]
+    result = Image.open(filename)
+    return result
+
+# places lego man on the left side of the blank image
+def pasteMan():
+    blankImage = Image.new('RGB',res,'white')
+    legoMan = getRandomMan(getAllImagesInDirectory())
+    legoMan = legoMan.resize(legoManRes)
+    blankImage.paste(legoMan,(0,0,legoManRes[0],legoManRes[1]))
+    return blankImage
+
+# crappy text wrap function
+# TODO: stop splitting words
+def convertToFit(phrase):
+    for counter, char in enumerate(phrase):
+        if counter % newlineCharacters == 0 and counter != 0:
+            phrase = phrase[:counter] + "\n" + phrase[counter:]
+    return phrase
+
+# draws text on image
+def makeImage():
+    img = pasteMan()
+    phrase = getPhrase()
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("segoesc.ttf", 25)
+    draw.text((150,10), convertToFit(phrase), font=font, fill=(0,0,0,0))
+                    
+    img.show()
